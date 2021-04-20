47db27b3f15dc7eeef2b54d9900931230c9847ae
bloody-badboy <arpan.asr30@gmail.com>
2021-04-20 20:48:44 +0530

    fix SimpleRVAdapter

--- SimpleRecycleViewAdapter.kt
+++ SimpleRecycleViewAdapter.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ${NAME} : RecyclerView.Adapter<${Model}Adapter.ViewHolder>() {
+class ${NAME} : RecyclerView.Adapter<${NAME}.ViewHolder>() {
 
     var data: MutableList<${Model}> = mutableListOf()
         set(value) {
@@ -17,10 +17,10 @@ class ${NAME} : RecyclerView.Adapter<${Model}Adapter.ViewHolder>() {
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.${Item_Layout_ID}, parent, false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position])
-    
+
     override fun getItemCount() = data.size
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ${Model}ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(item: ${Model}) = with(itemView) {
             // TODO: Bind the data with View
         }
